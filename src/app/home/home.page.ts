6caac50296f02d0d1a7647e8814f17f32a6d1c25
shoaib uddin <shoaibuddin12fx@gmail.com>
2022-07-06 20:37:54 +0500

    butons added

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,24 +1,33 @@
 import { Component } from '@angular/core';
 import { XsensCapacitor } from 'xsens-capacitor';
 
-
-
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-
   xsens;
+  // xse
   constructor() {
     this.xsens = XsensCapacitor;
-    this.initialize();
+    // this.initialize();
   }
 
-  async initialize(){
-    console.log()
-    this.xsens.echo({value: "POO"});
+  async initialize() {
+    const res = this.xsens.initialize();
+    console.log(res);
+
+    this.xsens.addListener('json_object', (json_object: any) => {
+      console.log('json_object was fired ', json_object);
+    });
+
+    // setTimeout(() => {
+    //   this.xsens.getBluetoothDevices();
+    // }, 4000);
   }
 
+  getBluetoothDevices() {
+    this.xsens.getBluetoothDevices();
+  }
 }
